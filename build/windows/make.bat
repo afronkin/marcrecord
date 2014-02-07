95231cd38d47bc61670c4e8dc9dfe037439bd112
Alexander Fronkin <alexander.fronkin@afronkin.net>
2014-02-07 12:01:02 +0400

    Updated Windws build

--- build/windows/make.bat
+++ build/windows/make.bat
@@ -1,2 +1,3 @@
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 nmake /f Makefile %*
